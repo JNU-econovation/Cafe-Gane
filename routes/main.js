64709d2568927918c74c1e8330205665161ffae6
kssumin <ksumin201566@naver.com>
2022-01-17 18:00:52 +0900

    initial commit

---
+++ routes/main.js
@@ -0,0 +1,13 @@
+var express=require('express');
+var router=express.Router();
+const path=require('path');
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname +'../../client/Start','Start.html'));
+
+});
+
+
+
+
+module.exports=router;
